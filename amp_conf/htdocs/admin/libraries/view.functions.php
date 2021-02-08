2b8cc97dce666b0838c62fbd148da06245de82c2
akbar khan <akhan@sangoma.com>
2021-02-08 14:09:45 +0000

    FREEI-2783 php upgrade to 7.4 depricated function

--- amp_conf/htdocs/admin/libraries/view.functions.php
+++ amp_conf/htdocs/admin/libraries/view.functions.php
@@ -11,12 +11,12 @@ function frameworkPasswordCheck() {
   //
 
 	// send error if magic_quotes_gpc is enabled on this system as much of the code base assumes not
-	//
-	if(get_magic_quotes_gpc()) {
-		$nt->add_error('core', 'MQGPC', _("Magic Quotes GPC"), _("You have magic_quotes_gpc enabled in your php.ini, http or .htaccess file which will cause errors in some modules. FreePBX expects this to be off and runs under that assumption"));
-	} else {
-		$nt->delete('core', 'MQGPC');
-	}
+	// get_magic_quotes_gpc is depricated by php from 7.4, as php no longer adds slashes to request parameters 
+	// if(get_magic_quotes_gpc()) {
+	// 	$nt->add_error('core', 'MQGPC', _("Magic Quotes GPC"), _("You have magic_quotes_gpc enabled in your php.ini, http or .htaccess file which will cause errors in some modules. FreePBX expects this to be off and runs under that assumption"));
+	// } else {
+	// 	$nt->delete('core', 'MQGPC');
+	// }
 }
 
 // setup locale
